1cf83dfc8432c7a16e6bb3b048837e4e113ef9ea
nikitakashin <rop-2012@bk.ru>
2022-06-26 14:21:36 +0300

    Global refactor

--- index.js
+++ index.js
@@ -1,94 +1,38 @@
 const axios = require("axios").default;
 
-const apiHost = "https://commondev.ru/";
-// const apiHost = "http://localhost:4000/";
+// const apiHost = "https://commondev.ru";
+const apiHost = "http://localhost:4000";
 
-// headers: {
-//   Authorization: "Bearer " + payload.token,
-// },
+const url = (method, version = "2.0") => {
+  return `${apiHost}/api/v${version}/${method}/`;
+};
 
-class ResultObject {
-  constructor(err, data) {
-    this.err = err;
-    this.data = data;
-  }
-
-  get() {
+const call = async (method, version, payload) => {
+  try {
+    const requestUrl = url(method, version);
+    const response = await axios.post(requestUrl, payload);
+    return response.data;
+  } catch (err) {
     return {
-      status: !!Object.entries(this.err).length,
-      err: this.err,
-      data: this.data,
+      status: "error",
+      data: err,
     };
   }
-}
-
-module.exports.v2 = {
-  apiVersion: "v2.0",
-
-  getUserByToken: async (payload) => {
-    const requestUrl = `${apiHost}/api/${this.apiVersion}/User.read/`;
-
-    try {
-      const response = await axios.post(requestUrl, {
-        token: payload.token,
-      });
-
-      return new ResultObject({}, response).get();
-    } catch (err) {
-      return new ResultObject(err, {}).get();
-    }
-  },
-
-  login: async (payload) => {
-    const requestUrl = `${apiHost}/api/${this.apiVersion}/User.login`;
-
-    try {
-      const response = await axios.post(requestUrl, {
-        phone: payload.phone,
-      });
-
-      return new ResultObject({}, response.data.data).get();
-    } catch (err) {
-      return new ResultObject(err, {}).get();
-    }
-  },
-
-  register: async (payload) => {
-    const requestUrl = `${apiHost}/api/${this.apiVersion}/User.register`;
-
-    try {
-      const response = await axios.post(requestUrl, {
-        phone: payload.phone,
-        firstname: payload.firstname,
-        lastname: payload.lastname,
-      });
-
-      if (response.data.status === "fail") {
-        return new ResultObject(response.data.data[0], {}).get();
-      } else {
-        return new ResultObject({}, response.data.data).get();
-      }
-    } catch (err) {
-      return new ResultObject(err, {}).get();
-    }
-  },
-
-  complete: async (payload) => {
-    const requestUrl = `${apiHost}/api/${this.apiVersion}/User.complete`;
-
-    try {
-      const response = await axios.post(requestUrl, {
-        code: payload.code,
-        phone: payload.phone,
-      });
-
-      if (response.status === "success") {
-        return new ResultObject({}, response?.data?.token).get();
-      } else {
-        return new ResultObject(response, {}).get();
-      }
-    } catch (err) {
-      return new ResultObject(err, {}).get();
-    }
-  },
 };
+
+const methodsMap = new Map([
+  ["getUserByToken", "User.read"],
+  ["login", "User.login"],
+  ["register", "User.register"],
+  ["complete", "User.complete"],
+]);
+
+let methods = {};
+
+methodsMap.forEach((value, key) => {
+  methods[key] = (payload) => {
+    return call(value, "2.0", payload);
+  };
+});
+
+module.exports.v2 = methods;
