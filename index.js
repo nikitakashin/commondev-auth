f77820958641fae1c8df1410cbd42fdbf149b707
nikitakashin <rop-2012@bk.ru>
2022-06-26 14:45:41 +0300

    Some fixes

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const axios = require("axios").default;
 
-// const apiHost = "https://commondev.ru";
-const apiHost = "http://localhost:4000";
+const apiHost = "https://commondev.ru";
+// const apiHost = "http://localhost:4000";
 
 const url = (method, version = "2.0") => {
   return `${apiHost}/api/v${version}/${method}/`;
 };
 
+class ResultObject {
+  constructor(err, data) {
+    this.err = err;
+    this.data = data;
+  }
+
+  get() {
+    return {
+      status: !Object.entries(this.err).length,
+      err: this.err.hasOwnProperty("name")
+        ? this.err.name === "AxiosError"
+          ? this.err.message === "Request failed with status code 500"
+            ? this.err.response.data.data
+            : { type: this.err.message }
+          : this.err
+        : this.err,
+      data: this.data.hasOwnProperty("data") ? this.data.data : this.data,
+    };
+  }
+}
+
 const call = async (method, version, payload) => {
   try {
     const requestUrl = url(method, version);
     const response = await axios.post(requestUrl, payload);
-    return response.data;
+    return new ResultObject({}, response.data).get();
   } catch (err) {
-    return {
-      status: "error",
-      data: err,
-    };
+    return new ResultObject(err, {}).get();
   }
 };
 
 const methodsMap = new Map([
-  ["getUserByToken", "User.read"],
+  ["getUserByToken", "User.getUserByToken"],
   ["login", "User.login"],
   ["register", "User.register"],
   ["complete", "User.complete"],
